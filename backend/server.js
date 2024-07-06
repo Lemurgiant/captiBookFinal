f06ebf7aee41437d32328a1b2006e963afac27a5
Marlon <erenyagami34@gmail.com>
2024-07-06 15:35:44 +0800

    trust proxy debug

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "15mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ limit: "15mb", extended: true }));
-
+app.set("trust proxy", 1);
 // Session setup
 app.use(
   session({
