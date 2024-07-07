936ea8bae2caecf0653254dffbe8f5434c1e0d97
Marlon <erenyagami34@gmail.com>
2024-07-07 16:38:57 +0800

    layout debug

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -3,15 +3,15 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import Sidebar from "../features/sidebar/Sidebar";
 
-const AppLayoutWrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-`;
+// const AppLayoutWrapper = styled.div`
+//   display: flex;
+//   height: 100vh;
+//   width: 100vw;
+// `;
 
 const MainBarWrapper = styled.main`
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   box-sizing: border-box !important;
   padding-top: 1.8rem;
   background-color: ${({ theme }) => theme.bg.dark};
@@ -27,10 +27,10 @@ interface AppLayoutProps {
 
 const AppLayout: React.FC<AppLayoutProps> = React.memo(({ children }) => {
   return (
-    <AppLayoutWrapper>
+    <>
       <Sidebar />
       <MainBarWrapper>{children ? children : <Outlet />}</MainBarWrapper>
-    </AppLayoutWrapper>
+    </>
   );
 });
 
