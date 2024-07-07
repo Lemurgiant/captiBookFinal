5480686645fbb1130c8f536870d916f34872c7e5
Marlon <erenyagami34@gmail.com>
2024-07-07 10:46:01 +0800

    rem debug

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -12,7 +12,9 @@ const AppLayoutWrapper = styled.div`
 const MainBarWrapper = styled.main`
   height: 100%;
   width: 100%;
-  box-sizing: border-box;
+  box-sizing: border-box !important;
+  padding: 3rem 0;
+  padding-left: 8rem;
   background-color: ${({ theme }) => theme.bg.dark};
   overflow-y: auto;
   display: flex;
